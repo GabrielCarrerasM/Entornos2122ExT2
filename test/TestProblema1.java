ae75a454470b54705ae8683b90f4dae037428b75
Puks <joseg.carrerasmiguel@iesfm.onmicrosoft.com>
2022-03-04 11:30:47 +0100

    añadido ejercicio2

--- test/TestProblema1.java
+++ test/TestProblema1.java
@@ -1,2 +1,31 @@
 public class TestProblema1 {
+
+    static String devuelveCadenaTexto()
+    {
+        String n = "";
+        int num1 = 50;
+        int num2 = 100;
+
+        for (int i = num1; i <= num2; i++)
+        {
+            if (i % 3 == 0)
+            {
+                n += "Fizz";
+            }
+            if (i % 5 == 0)
+            {
+                n += "Buzz";
+            }
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                n += "FizzBuzz";
+            }
+            else {
+                n += "" + i;
+            }
+            System.out.println(n);
+        }
+        return n;
+    }
+
 }
